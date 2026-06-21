f4198b00b8cc82e118ed301118f7d35a00023d65
Ram Kargwal <sudarshan8555@gmail.com>
2026-06-21 15:10:42 +0530

    Update Play_Quiz_Json_Data.ps1

--- Other Data/Code/SudarshanTools/FolderTools/Play_Quiz_Json_Data.ps1
+++ Other Data/Code/SudarshanTools/FolderTools/Play_Quiz_Json_Data.ps1
@@ -5,13 +5,13 @@ $result = Get-ChildItem -LiteralPath $Path -File | ForEach-Object {
     $name = [IO.Path]::GetFileNameWithoutExtension($_.Name)
 
     [PSCustomObject]@{
-        link = "file:///" + ($_.FullName -replace '\\','/')
         name = ($name -replace '_',' ')
+        link = "file:///" + ($_.FullName -replace '\\','/')
     }
 }
 
-# 1. डेटा को JSON में बदलकर क्लिपबोर्ड (Clipboard) में कॉपी करना
-($result | ConvertTo-Json -Compress) | Set-Clipboard
+# Pretty JSON clipboard में copy करें
+$result | ConvertTo-Json -Depth 3 | Set-Clipboard
 
-# 2. डेटा कॉपी होने के ठीक बाद HTML फाइल को डिफ़ॉल्ट ब्राउज़र में ओपन करना
+# HTML file open करें
 Start-Process "C:\SudarshanTools\html\Raman_Photo_quiz.html"
